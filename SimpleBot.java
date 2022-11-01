2805cfae9d1082b73f860e0045775644fdf11961
TiranLavanda <74424676+TiranLavanda@users.noreply.github.com>
2022-10-31 16:50:33 -0800

    Изменение имени

--- SimpleBot.java
+++ SimpleBot.java
@@ -6,7 +6,7 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Konor", "2020"); // change it as you need
+        greet("Sagan", "2022"); // change it as you need
         remindName();
         guessAge();
         count();
